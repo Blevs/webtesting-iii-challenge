90a3d63c89b0eeaf2a0fe4f94161bc52452e648d
Henry Blevins <heblevi@gmail.com>
2019-05-15 15:54:50 -0400

    Test Display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('initial state open unlocked', () => {
+    const { getByText } = render(<Display />);
+    expect(getByText('Unlocked'));
+    expect(getByText('Open'));
+  });
+  it('accept state props', () => {
+    const { getByText } = render(<Display locked={true} closed={true}/>);
+    expect(getByText('Locked'));
+    expect(getByText('Closed'));
+  });
+});
